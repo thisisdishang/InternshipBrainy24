f647e5c680c2165d938b9ee38e62256d9cc1eddd
thisisdishang <dishangkumarrana@gmail.com>
2024-05-25 14:28:25 +0530

    implement setOnClickListener event on login button and apply validation, toast, snackbar

--- app/src/main/java/edu/internship/brainy24/MainActivity.java
+++ app/src/main/java/edu/internship/brainy24/MainActivity.java
@@ -2,13 +2,56 @@ package edu.internship.brainy24;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button login;
+    TextView signup, forgotPassword;
+
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.main_username);
+        password = findViewById(R.id.main_password);
+        login = findViewById(R.id.main_login);
+        signup = findViewById(R.id.main_signup);
+        forgotPassword = findViewById(R.id.main_forgot);
+
+        forgotPassword.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().trim().equals("")) {
+                    username.setError("Username Required");
+                } else if (!username.getText().toString().trim().matches(emailPattern)) {
+                    username.setError("Valid Email ID Required");
+                } else if (password.getText().toString().trim().equals("")) {
+                    password.setError("Password Required");
+                } else if (password.getText().toString().trim().length() < 6) {
+                    password.setError("Minimum 6 Char Password Required");
+                } else {
+                    System.out.println("Login Successfully");
+                    Log.d("Dishang Response", "Login Successfully");
+                    Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                    Snackbar.make(view, "Login Successfully", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
