e1c2d4f562fcc6884ad0bdc35a51555460c5a2d0
thisisdishang <dishangkumarrana@gmail.com>
2024-05-27 12:41:09 +0530

    Implement radio button,spinner and checkbox in signup page

--- app/src/main/java/edu/internship/brainy24/SignupActivity.java
+++ app/src/main/java/edu/internship/brainy24/SignupActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -17,7 +23,17 @@ public class SignupActivity extends AppCompatActivity {
 
     Button signup;
 
+    //    RadioButton male, female;
+    RadioGroup gender;
+
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    Spinner city;
+    String[] cityArray = {"Select City", "Surat", "Vadodara", "Somnath", "Ahmedabad", "Bhuj", "Bhavnagar", "Rajkot", "Valsad"};
+
+    CheckBox terms;
+
+    String sCity = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +51,11 @@ public class SignupActivity extends AppCompatActivity {
         confirmpasswordShow = findViewById(R.id.signup_confirm_password_show);
         confirmpasswordHide = findViewById(R.id.signup_confirm_password_hidden);
         signup = findViewById(R.id.signup_button);
+//        male = findViewById(R.id.signup_male);
+//        female = findViewById(R.id.signup_female);
+        gender = findViewById(R.id.signup_gender);
+        city = findViewById(R.id.signup_city);
+        terms = findViewById(R.id.signup_terms);
 
         passwordShow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,7 +106,7 @@ public class SignupActivity extends AppCompatActivity {
                     email.setError("Valid Email ID Required");
                 } else if (contact.getText().toString().trim().equals("")) {
                     contact.setError("Contact Number Required");
-                } else if (contact.getText().toString().trim().length()!=10) {
+                } else if (contact.getText().toString().trim().length() != 10) {
                     contact.setError("Contact Number Must Have 10 Digit");
                 } else if (password.getText().toString().trim().equals("")) {
                     password.setError("Password Required");
@@ -97,11 +118,60 @@ public class SignupActivity extends AppCompatActivity {
                     confirmPassword.setError("Minimum 6 Character Password Required");
                 } else if (!password.getText().toString().trim().matches(confirmPassword.getText().toString().trim())) {
                     confirmPassword.setError("Password Does Not Match");
+                } else if (gender.getCheckedRadioButtonId() == -1) {
+                    new CommonMethod(SignupActivity.this, "Please Select Gender");
+                } else if (sCity.equals("")) {
+                    new CommonMethod(SignupActivity.this, "Please Select City");
+                } else if (!terms.isChecked()) {
+                    new CommonMethod(SignupActivity.this, "Please Accept Terms & Condition");
                 } else {
                     new CommonMethod(SignupActivity.this, "Signup Successfully");
                     new CommonMethod(view, "Signup Successfully");
                     onBackPressed();
                 }
+            }
+        });
+
+//        male.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                new CommonMethod(SignupActivity.this, "Male");
+//            }
+//        });
+//
+//        female.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                new CommonMethod(SignupActivity.this, "Female");
+//            }
+//        });
+
+//      Improve coding standard
+        gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton radioButton = findViewById(i);
+                new CommonMethod(SignupActivity.this, radioButton.getText().toString());
+            }
+        });
+
+        ArrayAdapter adapter = new ArrayAdapter(SignupActivity.this, android.R.layout.simple_list_item_checked, cityArray);
+        city.setAdapter(adapter);
+
+        city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0) {
+                    sCity = "";
+                } else {
+                    sCity = cityArray[i];
+                    new CommonMethod(SignupActivity.this, cityArray[i]);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
     }
